2b9db59a4fd0d176fbfc902c299bb12872a6957d
ngoswami1 <ngoswami@deqode.com>
2023-05-30 22:49:10 +0530

    Order book

--- src/app/order-book/order-book.component.ts
+++ src/app/order-book/order-book.component.ts
@@ -9,6 +9,8 @@ import { ChartDataService } from '../services/chart-data.service';
   styleUrls: ['./order-book.component.scss']
 })
 export class OrderBookComponent {
+  bookMap: Map<any,any> = new Map();
+  displayedColumns: string[] = ['']
   constructor(private chartDataService: ChartDataService) {
    
   }
@@ -19,7 +21,21 @@ export class OrderBookComponent {
       channel: 'book', 
       symbol: 'tBTCUSD' 
     }
-    this.chartDataService.getOrderBookData(msg).subscribe(data => {console.log("this is data", data)})
+    this.chartDataService.getOrderBookData(msg).subscribe(data => {
+      if(data.length == 3) {
+        console.log("this is data update", data);
+        if(data[1] != 0) {
+          this.bookMap.set(data[0], data);
+        } else {
+          this.bookMap.delete(data[0]);
+        }
+      } else {
+        console.log("this is data first", data);
+        data.forEach((element: any) => {
+          this.bookMap.set(element[0], element);
+        });
+      }
+    })
   }
 
   dataMapping(values: number[]) {
